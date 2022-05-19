fc3e6c0086348bcc323754dd31da245c51413762
willianchu <willianchu@hotmail.com>
2022-05-19 10:25:47 -0700

    Math.abs

---
+++ diagonalDiference.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+function diagonalDifference(arr) {
+    // Write your code here
+    const matrixLength = arr.length;
+    let diagonal1 = 0;
+    let diagonal2 = 0;
+    for (let i = 0; i < matrixLength; i++){
+      diagonal1 += arr[i][i];
+      diagonal2 += arr[matrixLength - i -1][i];
+    }
+    return Math.abs(diagonal1 - diagonal2); 
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    let arr = Array(n);
+
+    for (let i = 0; i < n; i++) {
+        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    const result = diagonalDifference(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
---
+++ lonelyInteger.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'lonelyinteger' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function lonelyinteger(a) {
+    const objects = countOccurrences(a);
+      for (let key in objects) {
+      if (objects[key] == 1) {
+        return key;
+      }
+    }
+
+}
+
+function countOccurrences(arr){
+  const counter = {};
+    arr.map(element => {
+      if (element in counter) {
+        counter[element] += 1;
+      } else {
+        counter[element] = 1;
+      }
+    })
+  return counter;  
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = lonelyinteger(a);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
