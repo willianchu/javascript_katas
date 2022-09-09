792de4acdfd4a25c143272113a4e7d631ff1c64c
willianchu <willianchu@hotmail.com>
2022-09-09 15:33:31 -0700

    FlatMap

--- flatMap.js
+++ flatMap.js
@@ -1,5 +1,31 @@
-let ar = [ 0,1,2,3,4,5,6,7,8,9]
-let n = 10;
-let k = 3;
+// first version
+const numbers = [0, 3, 6];
+const doubled = numbers
+  .filter(n => n !== 0)
+  .map(n => n * 2);
+console.log(doubled); // logs [6, 12]
 
-console.log(ar.flatMap())
+// with flatMap
+ numbers = [0, 3, 6];
+ doubled = numbers.flatMap(number => {
+  return number === 0 ? [] : [2 * number];
+});
+console.log(doubled); // logs [6, 12]
+
+
+// inserting values to array
+ numbers = [1, 4];
+const trippled = numbers.flatMap(number => {
+  return [number, 2 * number, 3 * number];
+});
+console.log(trippled);
+// logs [1, 2, 3, 4, 8, 12]
+
+
+// FLATTING an array
+const arrays = [[2, 4], [6]];
+const flatten = arrays.flatMap(item => item);
+
+console.log(flatten); // logs [2, 4, 6]
+
+
