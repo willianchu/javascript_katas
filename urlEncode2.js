a915f1b60059be4a365cfb589c694e10d95339e2
willianchu <willianchu@hotmail.com>
2022-09-10 09:50:42 -0700

    version 2

--- urlEncode2.js
+++ urlEncode2.js
@@ -1,14 +1,23 @@
+// function urlEncode(string) {
+//   // let core = string.trim(); // remove spaces at the beginning and end
+//   // core = core.replace(/ {2,}/g, " "); // remove repeated spaces
+
+//   // return encodeURIComponent(core); // simple way
+
+//   let core = string.split(' ')
+//                 .filter(word => word != '')
+//                 .join(' ');
+//   return core.split(' ').join('%20'); // more complex way
+// };
+
 function urlEncode(string) {
-  // let core = string.trim(); // remove spaces at the beginning and end
-  // core = core.replace(/ {2,}/g, " "); // remove repeated spaces
-
-  // return encodeURIComponent(core); // simple way
-
-  let core = string.split(' ')
-                .filter(word => word != '')
-                .join(' ');
-  return core.split(' ').join('%20'); // more complex way
+  return string.split(' ')
+    .filter(word => word != '')
+    .join(' ')
+    .split(' ')
+    .join('%20');
 };
+
 
 console.log(urlEncode("Lighthouse Labs"));
 console.log(urlEncode("   Lighthouse  Labs    "));
