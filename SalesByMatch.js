7c12754dca7fcb49399f44c4733be7ee6bf0c644
willianchu <willianchu@hotmail.com>
2022-09-14 11:01:33 -0700

    count ever 2 pair

---
+++ SalesByMatch.js
@@ -0,0 +1,28 @@
+function sockMerchant(n, ar) {
+  const inventory = {}
+  ar.forEach( color => {
+    if(inventory[color]){
+      inventory[color] ++;
+    } else {
+      console.log("new color: ",color);
+      inventory[color] = 1;
+    }
+  })
+  
+  return Object.keys(inventory).reduce( (total, key) =>{
+    return total + Math.floor(inventory[key]/2);
+  }, 0);
+ 
+}
+
+
+// array with the colors of socks
+let arr = [1,2,1,2,1,3,2];
+let n = arr.length;
+
+console.log( sockMerchant(n, arr) );
+
+let arr2 = [10, 20, 20, 10, 10, 30, 50, 10, 20];
+n = arr2.length;
+
+console.log( sockMerchant(n, arr2) );
