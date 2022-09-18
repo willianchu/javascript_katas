2f127db087e1cb7a85328ade95cd19fa56ebb7a2
willianchu <willianchu@hotmail.com>
2022-09-17 20:17:58 -0700

    fixed

--- DesignerPDFViewer.js
+++ DesignerPDFViewer.js
@@ -2,8 +2,9 @@ function designerPdfViewer(h, word) {
   const wordLower = word.toLowerCase();
   const wordLength = word.length;
   let maxHeight = 0;
+  let index = 0;
   for(let i = 0; i < wordLength; i++){
-    index = wordLower.charCodeAt(i);
+    index = wordLower.charCodeAt(i) - 97;
     if(maxHeight < h[index]){
       maxHeight = h[index];
     }
@@ -11,3 +12,7 @@ function designerPdfViewer(h, word) {
   return maxHeight * wordLength;
 }
 
+const h = [1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7];
+const word = "zaba";
+
+console.log(designerPdfViewer(h, word));
