9da43efcdcbf67bb154a3832171e50a80c116726
willianchu <willianchu@hotmail.com>
2022-09-19 10:58:38 -0700

    two indexes

--- isString.js
+++ isString.js
@@ -1,4 +1,11 @@
 function hackerrankInString(s) {
-  // Write your code here
-
+  const hackerrank = "hackerrank"
+  const size = s.length
+  let index = 0;
+  for(let i = 0; i < size; i++){
+    if(s[i]===hackerrank[index]){
+      index++;
+    }
+  }
+  return hackerrank.length === index ? "YES" : "NO";
 }
