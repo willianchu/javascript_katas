c4d9063ab7fab096322c448838cbd1d7502dac1f
willianchu <willianchu@hotmail.com>
2022-09-20 13:05:12 -0700

    typo error

--- LibraryFine.js
+++ LibraryFine.js
@@ -1,7 +1,14 @@
 function libraryFine(d1, m1, y1, d2, m2, y2) {
-  let fine = 0;
-  if(y1>y2) fine = 1000; 
-  if(m1>m2 && y1===y2) fine = 500 * (m1-m2);
-  if(d1>d2 && m1===m2) fine = 15 * (d1 - d2);
-  return fine;
+  if(y1>y2) return 10000;  // apply year fee
+  if(m1>m2 && y1===y2) return 500 * (m1-m2); // apply monthly fee
+  if(d1>d2 && m1===m2 && y1===y2) return 15 * (d1 - d2);
+  return 0;
 }
+
+// return y1 < y2 ? // abort if earlier
+//              0 : y1 > y2 ? // apply year fee
+//                    10000 : m1 > m2 && y1 === y2 ? // apply monthly fee
+//                                 (m1 - m2) * 500 : d1 > d2 && y1 === y2 && m1 === m2 ? // Apply daily
+//                                                                      (d1 - d2) * 15 : 0;
+
+console.log(libraryFine(1,1,2001,1,1,2000));
