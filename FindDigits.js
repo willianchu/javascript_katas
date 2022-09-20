3f4a1b7a96251ee38bf60598b0cfeef0458d6a3c
willianchu <willianchu@hotmail.com>
2022-09-20 11:07:02 -0700

    refactored

--- FindDigits.js
+++ FindDigits.js
@@ -1,22 +1,36 @@
 function findDigits(n) {
   let string = n + "";
   let arr = string.split("");
-  count = 0;
+  let count = 0;
   arr.forEach(digit=>{
     let number = digit * 1;
-    console.log(number);
-    switch(number){
-      case 0:
-        break;
-      case 1:
-        count++;
-        break;
-      default:
-        if(n % number === 0) count++;
+    if(number != 0){
+      if(n%number === 0) count++;
     }
   })
   return count;
 }
+
+
+// function findDigits(n) {
+//   let string = n + "";
+//   let arr = string.split("");
+//   let count = 0;
+//   arr.forEach(digit=>{
+//     let number = digit * 1;
+//     console.log(number);
+//     switch(number){
+//       case 0:
+//         break;
+//       case 1:
+//         count++;
+//         break;
+//       default:
+//         if(n % number === 0) count++;
+//     }
+//   })
+//   return count;
+// }
 
 console.log(findDigits(1230));
 
