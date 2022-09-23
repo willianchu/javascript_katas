4d8ab1633ecc4c189d4a51ddb7870333cd40738b
willianchu <willianchu@hotmail.com>
2022-09-23 06:46:33 -0700

    equalize answer

--- equalizeArray.js
+++ equalizeArray.js
@@ -1,4 +1,17 @@
 function equalizeArray(arr) {
-    // Write your code here
-
+  let obj = {};
+  for(let element of arr){
+      if(obj[element]){
+          obj[element]++;
+      } else {
+          obj[element]=1;
+      }
+  }
+  let values = Object.values(obj);
+  if(values.length === 1) return 0;
+  values.sort((a,b)=>a-b);
+  values.splice(-1);
+  return values.reduce((a, b) => a + b);
 }
+
+const arr =[51, 51, 51, 51, 51 ,51, 51 ,51 ,51 ,51 ,51 ,51, 51 ,51 ,51 ,51 ,51 ,51, 51, 51, 51, 51];
