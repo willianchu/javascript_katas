473d070312252128702d5439c05666a6774ab456
willianchu <willianchu@hotmail.com>
2022-09-23 10:52:33 -0700

    initialize functions first

--- AStar.js
+++ AStar.js
@@ -10,69 +10,34 @@ let map =[
 ];
 
 
+
 let FINISH = 3;
 let FREESPACE = 0;
 let BLOCKEDSPACE = 1;
 let mapSize = {
-  'maxX': map[0].length,
-  'maxY': map.length
+  'maxX': map[0].length - 1,
+  'maxY': map.length - 1
 };
 
 const findTheShortest = (initialX, initialY, finalX, finalY, map) => {
-  let FINALX = finalX;
-  let FINALY = finalY;
-  let openList = {};
-  let closeList = {};
-  openList[initialX+"x"+initialY] = {
-    'x': initialX,
-    'y': initialY,
-    'toStart': 0,
-    'toFinish': Math.abs(initialX - FINALX) + Math.abs(initialY - FINALY),
-    'cost': this.toStart + this.toFinish
-  };
-  let nextPosition = {};
-  let currentPositionKey = initialX+"x"+initialY;
 
-  do{
-    let noNewAdd = true;
-    nextPosition = canGoUp(openList[currentPositionKey]);
-    if(nextPosition != {}){
-      addToOpenList(nextPosition,currentPositionKey);
-      noNewAdd = false;
-    }
-    nextPosition = canGodown(openList[currentPositionKey]);
-    if(nextPosition != {}){
-      addToOpenList(nextPosition, currentPositionKey);
-      noNewAdd = false;
-    }
-    nextPosition = canGoLeft(openList[currentPositionKey]);
-    if(nextPosition != {}){
-      addToOpenList(nextPosition, currentPositionKey);
-      noNewAdd = false;
-    }
-    nextPosition = canGoRight(openList[currentPositionKey]);
-    if(nextPosition != {}){
-      addToOpenList(nextPosition, currentPositionKey);
-      noNewAdd = false;
-    }
-    if(noNewAdd){
-      moveToCloseList(currentPositionKey);
-    }
-    currentPositionKey = getSmallerCost(openList);
-  }while(currentPositionKey != "");
-
-  const canGoUp = (tile) => {
-    let up = tile.y - 1;
-    let yAxis = tile.y;
-    if(up < 0 ) return {};
-    if(map[up][yAxis]===BLOCKEDSPACE) return {}
-    if(map[up][yAxis]===FINISH) return {'end': true}
-    return {'x': up, 'y': yAxis }
+  const canGoUpDown = (tile, step) => {
+    let upDown = tile.y + step;
+    let xAxis = tile.x;
+    let tileKey = tile.x+"x"+tile.y;
+    if(openList[tileKey]) return {};
+    if(closeList[tileKey]) return {};
+    if(upDown < 0 || upDown > mapSize.maxY) return {};
+    if(map[xAxis][upDown]===BLOCKEDSPACE) return {}
+    if(map[xAxis][upDown]===FINISH) return {'end': true}
+    return {'x': xAxis, 'y': upDown }
   }
-  const canGodown = (tile) => {
-    let down = tile.x + 1;
+  const canGoLeftRight = (tile, step) => {
+    let leftRight = tile.x + 1;
     let yAxis = tile.y;
-    if(down > mapSize.maxX ) return {};
+    if(openList[tileKey]) return {};
+    if(closeList[tileKey]) return {};
+    if(leftRight < 0 || leftRight > mapSize.maxX ) return {};
     if(map[down][yAxis]===BLOCKEDSPACE) return {}
     if(map[down][yAxis]===FINISH) return {'end': true}
     return {'x': tile.x-1, 'y': tile.y }
@@ -102,6 +67,63 @@ const findTheShortest = (initialX, initialY, finalX, finalY, map) => {
     closeList[key] = openList[key];
     delete openList[key];
   }
+  
+  let FINALX = finalX;
+  let FINALY = finalY;
+  let openList = {};
+  let closeList = {};
+  openList[initialX+"x"+initialY] = {
+    'x': initialX,
+    'y': initialY,
+    'toStart': 0,
+    'toFinish': Math.abs(initialX - FINALX) + Math.abs(initialY - FINALY),
+    'cost': this.toStart + this.toFinish
+  };
+  let nextPosition = {};
+  let currentPositionKey = initialX+"x"+initialY;
 
+  do{
+    let noNewAdd = true;
+    nextPosition = canGoUpDown(openList[currentPositionKey],-1);
+    if(nextPosition != {}){
+      if(nextPosition.end) break;
+      addToOpenList(nextPosition,currentPositionKey);
+      noNewAdd = false;
+    }
+    console.log("go up",nextPosition);
+    nextPosition = canGoUpDown(openList[currentPositionKey],1);
+    if(nextPosition != {}){
+      if(nextPosition.end) break;
+      addToOpenList(nextPosition, currentPositionKey);
+      noNewAdd = false;
+    }
+    console.log("go down",nextPosition);
+    nextPosition = canGoLeftRight(openList[currentPositionKey], -1);
+    if(nextPosition != {}){
+      if(nextPosition.end) break;
+      addToOpenList(nextPosition, currentPositionKey);
+      noNewAdd = false;
+    }
+    console.log("go left",nextPosition);
+    nextPosition = canGoLeftRight(openList[currentPositionKey], 1);
+    if(nextPosition != {}){
+      if(nextPosition.end) break;
+      addToOpenList(nextPosition, currentPositionKey);
+      noNewAdd = false;
+    }
+    console.log("go right",nextPosition);
+    if(noNewAdd){
+      moveToCloseList(currentPositionKey);
+      console.log("close list",closeList);
+    }
+    
+    currentPositionKey = getSmallerCost(openList);
+    console.log("new current position",currentPositionKey);
+    console.log("open list",openList);
+  }while(currentPositionKey != "");
+
+  console.log(currentPositionKey);
+  return;
 }
 
+findTheShortest(0,0,7,7,map);
