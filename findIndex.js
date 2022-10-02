47d246bbf2fd5a4435181531d03654d52d93fb79
willianchu <willianchu@hotmail.com>
2022-10-01 21:31:25 -0700

    refactored

--- findIndex.js
+++ findIndex.js
@@ -3,25 +3,31 @@
   
 // }
 
-function sortInsertion (n, arr){
-  const zeroIndexLength = n - 1;
+function sortInsertionLast (n, arr){
+  const zeroIndexLength = n;
   const putItInOrder = arr[zeroIndexLength];
   let lastIndex = 0;
   for(let i=zeroIndexLength-1; i >= 0; i--){
     if(putItInOrder < arr[i]){
       arr[i+1] = arr[i];
-      console.log(arr);
     } else {
       lastIndex = i+1;
       break;
     }
   }
   arr[lastIndex] = putItInOrder;
-  console.log(arr);
 }
 
-let arr = [ 2,3,4,5,1 ];
+function insertionSort2(n, arr) {
+  for(let i = 1; i < n; i++){
+    sortInsertionLast(i,arr);
+    console.log(...arr);
+  }
+}
 
-let n = 5;
 
-sortInsertion(n,arr);
+let arr = [ 3,4,7,5,6,2,1 ];
+
+let n = 7;
+
+grow2Sort(n,arr);
