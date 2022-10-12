7bda560e678d85f604132f687d10bb4fbb3f2236
willianchu <willianchu@hotmail.com>
2022-10-12 12:35:41 -0700

    palindrome anagram checker

---
+++ gameOfThrones.js
@@ -0,0 +1,14 @@
+function gameOfThrones(s) {
+  let counter = {}
+  for(let letter of s){
+    counter[letter] = counter[letter] ? ++counter[letter] : 1;  
+  }
+  let tolerance = 2;
+  for(let key in counter){
+    if(counter[key]%2 != 0){
+      tolerance --;
+      if(tolerance===0) return false;
+    }
+  }
+  return true;
+}
