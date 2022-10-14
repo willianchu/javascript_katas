977c59b36ca6bf71f08efc2be157e4f56bb75bb3
willianchu <willianchu@hotmail.com>
2022-10-13 21:45:40 -0700

    debug

--- cavityMap.js
+++ cavityMap.js
@@ -1,4 +1,25 @@
 function cavityMap(grid) {
-    // Write your code here
-
+    let newMap = [];
+    let xValue = 0;
+    newMap[0] = grid[0];
+    for(let line = 1; line < grid.length -1; line++){
+      newMap[line] = grid[line][0];
+      for(let column = 1; column <  grid[line].length-1; column++ ){
+        xValue = grid[line][column];
+        console.log(xValue, line, column);
+        if(grid[line][column-1] < xValue 
+          && grid[line][column+1] < xValue 
+          && grid[line-1][column] < xValue 
+          && grid[line+1][column] < xValue){
+            newMap[line] += "X";
+          } else {
+            newMap[line] +=  xValue;
+          }
+          newMap[line] += grid[line][column+1];
+      }
+    }
+    newMap[grid.length-1] = grid[grid.length-1];
+    return newMap;
 }
+let map = ["1112","1912","1892","1234"];
+console.log(cavityMap(map));
