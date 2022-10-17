683364c1171443fbfa4b7e08d8a0b1cbc4b421ca
willianchu <willianchu@hotmail.com>
2022-10-17 15:20:17 -0700

    binary operation or

--- binaryOperations.js
+++ binaryOperations.js
@@ -1,7 +1,12 @@
-function bin2dec(bin){
-  return parseInt(bin, 2);
+function binOr(b1, b2){
+  let result = "";
+  let max = Math.max(b1.length, b2.length);
+  for(let i = 0; i < max; i++){
+    result = result + (b1[i] | b2[i]);
+  }
+  return result;
 }
 
 let bin1 = "01010101";
 let bin2 = "10101010";
-console.log((bin2dec(bin1) | bin2dec(bin2)).toString(2));
+console.log(binOr(bin1, bin2));
