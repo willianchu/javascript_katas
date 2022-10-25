9c839d68dae5f010e033ca13177942603ac8f817
willianchu <willianchu@hotmail.com>
2022-10-24 20:30:00 -0700

    Mars Exploration algorithm

--- binaryOperations.js
+++ binaryOperations.js
@@ -1,12 +1,12 @@
 function binOr(b1, b2){
-  let result = "";
+  let result = 0;
   let max = Math.max(b1.length, b2.length);
   for(let i = 0; i < max; i++){
-    result = result + (b1[i] | b2[i]);
+    if(b1[i] | b2[i]) result += 1;
   }
   return result;
 }
 
 let bin1 = "01010101";
-let bin2 = "10101010";
+let bin2 = "010101010";
 console.log(binOr(bin1, bin2));
---
+++ marsExploration.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'marsExploration' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+function marsExploration(s) {
+    
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const result = marsExploration(s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
