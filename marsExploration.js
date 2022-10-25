ddbdc86efe17d60d859e443e60a280dead7d0e39
willianchu <willianchu@hotmail.com>
2022-10-24 20:40:21 -0700

    final version

--- marsExploration.js
+++ marsExploration.js
@@ -1,47 +1,12 @@
-'use strict';
-
-const fs = require('fs');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-/*
- * Complete the 'marsExploration' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts STRING s as parameter.
- */
-
 function marsExploration(s) {
-    
+    let count = 0;
+    for(let i = 0; i < s.length; i++){
+        // console.log('s[i]:', s[i],"SOS[i % 3]:", "SOS"[i % 3], i,[i % 3]);
+        if(s[i] !== "SOS"[i % 3]) count++;
+    }
+    return count;    
 
 }
 
-function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+marsExploration("SOSSPSSQSSOR");
 
-    const s = readLine();
-
-    const result = marsExploration(s);
-
-    ws.write(result + '\n');
-
-    ws.end();
-}
