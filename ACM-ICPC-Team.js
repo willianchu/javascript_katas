b2a1b711d78f342987038f57c052a931874fb1e9
willianchu <willianchu@hotmail.com>
2022-10-28 18:54:47 -0700

    acm working

--- ACM-ICPC-Team.js
+++ ACM-ICPC-Team.js
@@ -7,23 +7,25 @@
  */
 
 function acmTeam(topic) {
-    let arr = [];
-    let members = topic.length;
-    for(let i = 0; i < members-1; i++){
-        arr[i]=[];
-        for(let j = i+1; j < members; j++){
-            let sizeTopic = topic[j].length;
-            for(let item in sizeTopic){
-                console.log(topic[i][item]);
-                if(topic[i][item]==='1' || topic[j][item]==='1'){
-                    arr[i].push(item+1);
+    let max = 0;
+    let count = 0;
+    for (let i = 0; i < topic.length; i++) {
+        for (let j = i + 1; j < topic.length; j++) {
+            let temp = 0;
+            for (let k = 0; k < topic[i].length; k++) {
+                if (topic[i][k] == '1' || topic[j][k] == '1') {
+                    temp++;
                 }
             }
+            if (temp > max) {
+                max = temp;
+                count = 1;
+            } else if (temp == max) {
+                count++;
+            }
         }
-        
     }
-    console.log(arr);
-    return arr;
+    return [max, count];
 }
 
 const topic = ["10101", "11100", "11010", "00101"];
