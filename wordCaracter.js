2bb27e5763436398271739f4e22c0a7a872f15e8
willianchu <willianchu@hotmail.com>
2022-11-11 13:56:51 -0800

    Regex for word caracters /w

---
+++ wordCaracter.js
@@ -0,0 +1,7 @@
+var Regex_Pattern = /\w{3}\W\w{10}\W\w{3}/; //Do not delete '/'. Replace __________ with your regex.
+
+// Task
+
+// You have a test string S . Your task is to match the pattern
+// xxxXxxxxxxxxxxXxxx 
+// Here 'x' denotes any word character and X denotes any non-word character.
