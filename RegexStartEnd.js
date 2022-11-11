f4b00b6491ac9a162ac203cc0f863ce0d4d1c482
willianchu <willianchu@hotmail.com>
2022-11-11 14:04:08 -0800

    regex Start and end

---
+++ RegexStartEnd.js
@@ -0,0 +1,12 @@
+var Regex_Pattern = /^\d\w\w\w\w\.$/;  //Do not delete '/'. Replace __________ with your regex.
+
+// The ^ symbol matches the position at the start of a string.
+
+// Task
+
+// You have a test string S. Your task is to match the pattern Xxxxx.
+// Here, 'x' denotes a word character, and 'X' denotes a digit.
+// S must start with a digit X and end with '.' symbol.
+// S should be 6 characters long only.
+
+// start with ^ and end with $ to match the entire string
