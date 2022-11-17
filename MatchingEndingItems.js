acc8571e1fdffe2d273831171d0aca8f4ca4d722
willianchu <willianchu@hotmail.com>
2022-11-16 17:13:11 -0800

    $ attach to the end

---
+++ MatchingEndingItems.js
@@ -0,0 +1,8 @@
+var Regex_Pattern = /__________/; //Do not delete '/'. Replace __________ with your regex.
+
+// Task
+
+// Write a RegEx to match a test string, S, under the following conditions:
+
+// S should consist of only lowercase and uppercase letters (no numbers or symbols). ---> [a-zA-Z]
+// S should end in s. ---> s$
