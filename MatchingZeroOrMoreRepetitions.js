2d17476119b27a6135c6acc3adf1f9bce1e705d0
willianchu <willianchu@hotmail.com>
2022-11-16 16:59:12 -0800

    update

--- MatchingZeroOrMoreRepetitions.js
+++ MatchingZeroOrMoreRepetitions.js
@@ -5,8 +5,8 @@ var Regex_Pattern = /__________/; //Do not delete '/'. Replace __________ with y
 // You have a test string S.
 // Your task is to write a regex that will match S using the following conditions:
 
-// S should begin with 2 or more digits.
-// After that, S should have 0 or more lowercase letters.
-// S should end with 0 or more uppercase letters
+// S should begin with 2 or more digits. ---> ^\d{2,}
+// After that, S should have 0 or more lowercase letters. ---> [a-z]*
+// S should end with 0 or more uppercase letters ---> [A-Z]*$
 
-Regex_Pattern = /^\d\d+[a-z]*[A-Z]*$/; 
+Regex_Pattern = /^\d{2,}[a-z]*[A-Z]*$/; 
