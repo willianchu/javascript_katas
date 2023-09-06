05cca9f08580922138fc32c5e8808724a8af84a0
willianchu <willianchu@hotmail.com>
2023-09-06 09:57:15 -0700

    rotate left with math optimization

---
+++ rotate_left.js
@@ -0,0 +1,61 @@
+// Given square array, rotate 90 degrees counter clockwise (left)
+
+let array = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+let answer = [
+  [],
+  [],
+  []
+];
+
+function rotate(array, nTimes) {
+  if(nTimes % 4 === 0) {
+    return array;
+  }
+  if(nTimes % 2 === 0) {
+    return upsideDown(array);
+  }
+  if((nTimes + 1) % 4 === 0) {
+    return rotateRight(array);
+  }
+  if((nTimes + 1) % 2 === 0) {
+    return rotateLeft(array);
+  }
+}
+
+const rotateRight = (array) => {
+  let lines = array.length;
+  let columns = array[0].length;
+  for(let c = 0; c < columns; c++) {
+    for(let l = lines - 1; l >= 0; l--) {
+      answer[c].push(array[l][c]);
+    }
+  }
+  return answer;
+}
+
+const rotateLeft = (array) => {
+  let lines = array.length;
+  let columns = array[0].length;
+  for(let c = columns - 1; c >= 0; c--) {
+    for(let l = 0; l < lines; l++) {
+      answer[c].push(array[l][c]);
+    }
+  }
+  return answer;
+}
+const upsideDown = (array) => {
+  let lines = array.length;
+  let columns = array[0].length;
+  for(let l = lines - 1; l >= 0; l--) {
+    for(let c = columns - 1; c >= 0; c--) {
+      answer[l].push(array[l][c]);
+    }
+  }
+  return answer;
+}
+
+console.log(rotate(array, 0));
